8998857b91ab69c89a9ddfc109963993b90c597a
SeeSchloss <see.github@seos.fr>
2016-06-28 19:51:39 +0200

    fake-getaddrinfo and fake-getnameinfo are useless now

--- src/http.c
+++ src/http.c
@@ -90,11 +90,7 @@ int http_close(SOCKET fd);
 /* pas de fioritures */
 #include <ctype.h>
 #include "inet_aton.h"
-#include "fake-getaddrinfo.h"
-#include "fake-getnameinfo.h"
 #include "inet_aton.c"
-#include "fake-getaddrinfo.c"
-#include "fake-getnameinfo.c"
 
 static void 
 dns_cache_destroy(HostEntry *h) {
