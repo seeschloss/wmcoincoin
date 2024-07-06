seeschloss/wmcoincoin

commit 3ef4e8bb1db9c8369b08806eda4ae505cff04261

Support references to post ids

src/pinnipede.c
@@ -691,13 +691,19 @@ pv_tmsgi_parse(Pinnipede *pp, Board *board, board_msg_info *mi, int with_seconds
         check_for_horloge_ref(board->boards, mi->id, s,attr_s, PVTP_SZ, &is_ref, NULL);
         if (is_ref) {
           attr |= PWATTR_REF;
-        }
 
-        if (strlen(s) > 10) {
-            if (s[10] == 'T') {
-                // les horloges de devnewton< indiquent la date à tort et à travers
-                // on va simplement ne pas l'afficher.
-                strcpy(s, s + 11);
+          if (strlen(s) > 10) {
+              if (s[10] == 'T') {
+                  // les horloges de devnewton< indiquent la date à tort et à travers
+                  // on va simplement ne pas l'afficher.
+                  strcpy(s, s + 11);
+              }
+          }
+        } else {
+            check_for_id_ref(board->boards, mi->id, s, attr_s, PVTP_SZ, &is_ref);
+
+            if (is_ref) {
+              attr |= PWATTR_REFID;
             }
         }
       }
@@ -739,7 +745,7 @@ pv_tmsgi_parse(Pinnipede *pp, Board *board, board_msg_info *mi, int with_seconds
 
       pw->next = pw_create(s, raw, attr, (attr & PWATTR_LNK) ? attr_s : NULL, pv);
       has_initial_space = 0;
-      attr &= ~(PWATTR_REF | PWATTR_VISITED | PWATTR_TOTOZ) ;
+      attr &= ~(PWATTR_REF | PWATTR_REFID | PWATTR_VISITED | PWATTR_TOTOZ) ;
       //      printf("ADD(id=%d): '%s'\n", mi->id, s);
       pw = pw->next;
     }
@@ -1245,7 +1251,7 @@ pp_draw_line(Dock *dock, Pixmap lpix, PostWord *pw,
       if (pw->attr & PWATTR_TSTAMP) {
 	color = pp->timestamp_color[site_num];
 //	if (pw->parent->is_my_message) { pixel = IRGB2PIXEL(0x000080); }
-      } else if (pw->attr & (PWATTR_LNK|PWATTR_REF)) {
+      } else if (pw->attr & (PWATTR_LNK|PWATTR_REF|PWATTR_REFID)) {
 	if ((pw->attr & PWATTR_VISITED) == 0) {
 	  color = pp->lnk_color[site_num];
 	} else {
@@ -1331,7 +1337,7 @@ pp_draw_line(Dock *dock, Pixmap lpix, PostWord *pw,
   return pw;
 }
 
-void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t timestamp, int sub_timestamp) {
+void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t timestamp, int sub_timestamp, board_msg_info *mi) {
   int l;
 
   for (l=0; l < pp->nb_lignes; l++) {
@@ -1343,7 +1349,15 @@ void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t time
       pw = pp->lignes[l];
       pl = pw->ligne;
       while (pw && pl == pw->ligne) {
-	if (pw->attr & PWATTR_REF) {
+	if (pw->attr & PWATTR_REFID) {
+	  int is_ref;
+
+	  ref2_mi = check_for_id_ref(boards, pw->parent->id, pw->raw, NULL, 0, &is_ref);
+      assert(is_ref);
+	  if (ref2_mi && mi && mi->id.lid == ref2_mi->id.lid) {
+          pw->attr |= PWATTR_TMP_EMPH;
+	  }
+	} else if (pw->attr & PWATTR_REF) {
 	  int bidon, ref2_num;
 
 	  ref2_mi = check_for_horloge_ref(boards, pw->parent->id, pw->raw, NULL, 0, &bidon, &ref2_num); assert(bidon);
@@ -1431,6 +1445,10 @@ pp_refresh(Dock *dock, Drawable d, PostWord *pw_ref)
     int is_ref;
     ref_mi = check_for_horloge_ref(boards, pw_ref->parent->id, pw_ref->raw, ref_comment, 200, &is_ref, &ref_num);
     assert(is_ref);
+  } else if (pw_ref && (pw_ref->attr & PWATTR_REFID)) {
+    int is_ref;
+    ref_mi = check_for_id_ref(boards, pw_ref->parent->id, pw_ref->raw, ref_comment, 200, &is_ref);
+    assert(is_ref);
   } else if (pw_ref && (pw_ref->attr & PWATTR_LNK)) {
     char *link = strdup(pw_ref->attr_s);
     char *anchor = strrchr(link, '#');
@@ -1444,7 +1462,7 @@ pp_refresh(Dock *dock, Drawable d, PostWord *pw_ref)
     free(link);
   } else if (pw_ref && (pw_ref->attr & PWATTR_TSTAMP)) {
     pp_refresh_hilight_refs(pp, boards, id_type_sid(pw_ref->parent->id), 
-			    pw_ref->parent->tstamp, pw_ref->parent->sub_tstamp);
+			    pw_ref->parent->tstamp, pw_ref->parent->sub_tstamp, pw_ref->parent);
   }
   if (ref_mi) { 
     board_msg_info *mi;
@@ -1479,7 +1497,7 @@ pp_refresh(Dock *dock, Drawable d, PostWord *pw_ref)
     /* et maintenant on detecte toutes les autres references vers ce message pour les afficher
        temporairement en gras (ça c vraiment pour faire le cakos)*/
     pp_refresh_hilight_refs(pp, boards, id_type_sid(ref_mi->id), 
-                            ref_mi->timestamp, ref_mi->sub_timestamp);
+                            ref_mi->timestamp, ref_mi->sub_timestamp, ref_mi);
   }
 
   /* 
@@ -2463,6 +2481,8 @@ pp_check_survol(Dock *dock, PostWord *pw, int force_refresh)
       free(stime);
       is_a_ref = 1;
     } else if (pw->attr & PWATTR_REF) {
+      is_a_ref = 1;
+    } else if (pw->attr & PWATTR_REFID) {
       is_a_ref = 1;
     }
   }

src/board_util.c
@@ -700,7 +700,27 @@ board_get_tok(const unsigned char **p, const unsigned char **np,
     }
     start++;
     if (*start) end = start+1;
+  } else if (*start == '#') { /* une référence à un post id */
+      do {
+          end++;
+      } while ((*end && isdigit(*end)) || (end-start > 16));
+
+      if (end-start == 3 && *(end) == ':') {
+          // en fait, c'est probablement juste une vieille horlage avec date de wmcc/olcc
+          end = start;
+
+          // euh...
+          goto timestamp;
+      }
+
+      if (*end == '@') {
+          // alors là y a un @tribune indiqué
+          do {
+              end++;
+          } while (isalnum(*end) || *end == '_');
+      }
   } else {
+timestamp:
     /* pour aider la reconnaissance des timestamp */
     if (*end >= '0' && *end <= '9') {
       unsigned char last = *end;
@@ -965,6 +985,129 @@ check_for_horloge_ref(Boards *boards, id_type caller_id,
 				  h, m, s, num, year, month, day, commentaire, comment_sz);
   } else return NULL;
 }
+
+/*
+  recherche un message par id
+  /!\ la recherche est locale à la board indiquée en parametre
+*/
+static board_msg_info *
+board_find_horloge_id(Board *board, id_type caller_id,
+		       int post_id, unsigned char *commentaire, int comment_sz)
+{
+  board_msg_info *mi;
+  mi = board->msg;
+
+  board_msg_info *found_mi = NULL;
+
+  while (mi) {
+    if (mi->id.lid == post_id) {
+        found_mi = mi;
+        break;
+    }
+
+    mi = mi->next;
+  }
+
+  if (commentaire) {
+      if (!found_mi) {
+          if (post_id > caller_id.lid) {
+              snprintf(commentaire, comment_sz, _("IPOT(tm) detected"));
+          } else {
+              snprintf(commentaire, comment_sz, _("but where is '%d' ?"), post_id);
+          }
+      }
+  }
+
+  return found_mi;
+}
+
+/* si 'ww' contient une reference (du type '#1630') vers un message existant, on renvoie 
+   son msg_info, et on rempli 'commentaire' 
+
+   site_name_hash contient 0 quand il n'est pas fait allusion à un site particulier (du genre #1630@linuxfr)
+*/
+static int
+check_for_id_ref_basic_helper(const unsigned char *ww, const char **site_name, int *ref_id)
+{
+  int l, id;
+
+  l = strlen(ww);
+
+  if (l < 2) return 0; /* de qui se moque-t-on ? */
+
+  if (ww[0] != '#') {
+      return 0;
+  }
+
+  /* on tente d'abord d'intercepter le nom du site */
+  *site_name = strchr(ww, '@');
+  if (*site_name) {
+    *site_name += 1;
+  }
+
+  id = atoi(ww + 1);
+
+  if (!id) {
+      return 0;
+  }
+
+  *ref_id = id;
+
+  return 1;
+}
+
+static int
+check_for_id_ref_basic(Boards *boards, const unsigned char *ww, int *site_id, int *ref_id)
+{
+  const char *site_name;
+  int ret;
+
+  *site_id = -1;
+  ret = check_for_id_ref_basic_helper(ww, &site_name, ref_id);
+  if (ret && site_name) { /* bon .. ça mérite qu'on cherche le site */
+    int i, hash;
+
+    for (i=0; isalpha(site_name[i]); i++) ;
+
+    hash = str_hache_nocase(site_name, i);
+    for (i = 0; i < boards->nb_aliases; i++) {
+      if (boards->aliases[i].hash == hash) {
+	*site_id = boards->aliases[i].sid;
+	break;
+      }
+    }
+    if (*site_id == -1) *site_id = -2; /* pour savoir qu'un @site a été utilisé, mais qu'il est mauvais */
+  }
+  return ret;
+}
+
+/* look for a reference on every board */
+board_msg_info *
+check_for_id_ref(Boards *boards, id_type caller_id, 
+		      const unsigned char *ww, unsigned char *commentaire, 
+		      int comment_sz, int *is_a_ref)
+{
+  Board *board;
+  int site_id, post_id;
+  
+  assert(!id_type_is_invalid(caller_id));
+  *is_a_ref = 0;
+  if (check_for_id_ref_basic(boards, ww, &site_id, &post_id) == 0) return NULL;
+  *is_a_ref = 1;
+
+  if (site_id >= 0) {
+    board = boards->btab[site_id];
+  } else if (site_id == -1) { /* on prend le site par défaut */
+    board = boards->btab[caller_id.sid];
+  } else {
+    snprintf(commentaire, comment_sz, _("I don't fucking know %s"), ww);
+    return NULL;
+  }
+  if (board) {
+    return board_find_horloge_id(board, caller_id, post_id, commentaire, comment_sz);
+  } else return NULL;
+}
+
 
 /* appelé discretement par board_check_my_messages dans board.c
    (oui c pas logique) 

src/board_util.h
@@ -15,6 +15,9 @@ int board_msg_is_ref_to_me(Boards *boards, const board_msg_info *ref_mi);
 board_msg_info *check_for_horloge_ref(Boards *boards, id_type caller_id, 
 		      const unsigned char *ww, unsigned char *commentaire, 
 		      int comment_sz, int *is_a_ref, int *ref_num);
+board_msg_info *check_for_id_ref(Boards *boards, id_type caller_id, 
+		      const unsigned char *ww, unsigned char *commentaire, 
+		      int comment_sz, int *is_a_ref);
 
 void board_msg_find_refs(Board *board, board_msg_info *mi);
 KeyList *board_key_list_cleanup(Boards *boards, KeyList *first);

src/pinnipede.h
@@ -34,6 +34,8 @@
 #define PWATTR_TOTOZ_FOUND (1<<18)
 #define PWATTR_TOTOZ (PWATTR_TOTOZ_UNKNOWN|PWATTR_TOTOZ_DOWNLOADING|PWATTR_TOTOZ_NOTFOUND|PWATTR_TOTOZ_FOUND)
 
+#define PWATTR_REFID (1<<19)
+
 
 #define PP_TOTOZ_STATUS_UNKNOWN 0
 #define PP_TOTOZ_STATUS_DOWNLOADING 1

commit b5e75ecf2a3d71e8b5a9143897f7191dbc730475

Fix passing of incompatible pointer arguments
`pp_refresh_hilight_refs` expects a pointer to a `board_msg_info` as its last
argument, but `pp_refresh` passes a point to a `struct _PostVisual`.  The
argument is used to get its `id` member, so make the parameter an `id_type`
pointer instead, and update the callers.

Fixes: 3ef4e8bb1db9 ("Support references to post ids")
Signed-off-by: Jeremy Sowden <jeremy@azazel.net>

src/pinnipede.c
@@ -1337,7 +1337,7 @@ pp_draw_line(Dock *dock, Pixmap lpix, PostWord *pw,
   return pw;
 }
 
-void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t timestamp, int sub_timestamp, board_msg_info *mi) {
+void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t timestamp, int sub_timestamp, id_type *mi_id) {
   int l;
 
   for (l=0; l < pp->nb_lignes; l++) {
@@ -1354,7 +1354,7 @@ void pp_refresh_hilight_refs(Pinnipede *pp, Boards *boards, int sid, time_t time
 
 	  ref2_mi = check_for_id_ref(boards, pw->parent->id, pw->raw, NULL, 0, &is_ref);
       assert(is_ref);
-	  if (ref2_mi && mi && mi->id.lid == ref2_mi->id.lid) {
+	  if (ref2_mi && mi_id->lid == ref2_mi->id.lid) {
           pw->attr |= PWATTR_TMP_EMPH;
 	  }
 	} else if (pw->attr & PWATTR_REF) {
@@ -1462,7 +1462,7 @@ pp_refresh(Dock *dock, Drawable d, PostWord *pw_ref)
     free(link);
   } else if (pw_ref && (pw_ref->attr & PWATTR_TSTAMP)) {
     pp_refresh_hilight_refs(pp, boards, id_type_sid(pw_ref->parent->id), 
-			    pw_ref->parent->tstamp, pw_ref->parent->sub_tstamp, pw_ref->parent);
+			    pw_ref->parent->tstamp, pw_ref->parent->sub_tstamp, &pw_ref->parent->id);
   }
   if (ref_mi) { 
     board_msg_info *mi;
@@ -1497,7 +1497,7 @@ pp_refresh(Dock *dock, Drawable d, PostWord *pw_ref)
     /* et maintenant on detecte toutes les autres references vers ce message pour les afficher
        temporairement en gras (ça c vraiment pour faire le cakos)*/
     pp_refresh_hilight_refs(pp, boards, id_type_sid(ref_mi->id), 
-                            ref_mi->timestamp, ref_mi->sub_timestamp, ref_mi);
+                            ref_mi->timestamp, ref_mi->sub_timestamp, &ref_mi->id);
   }
 
   /* 